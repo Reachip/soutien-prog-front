a3fae5d75d80bb007ba80e1cd3c9bd61cf94e6d3
Rached <r.mejri74100@gmail.com>
2021-06-27 22:59:11 +0200

    fix: rewriting states handling

--- components/Band.vue
+++ components/Band.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-white p-3" 
-        v-bind:class="[this.bg]" 
+        v-bind:class="[this.$store.state.band.bgProp]" 
         v-show="!this.$store.state.band.bandIsInvisible">
         <div class="flex justify-between">
             <p class="font-semibold text-center mx-8">{{this.$store.state.band.text}}</p>
@@ -10,25 +10,10 @@
 </template>
 <script>
 export default {
-    data: function() {
-        return {
-            bg: null
-        }
-    },
-
-    mounted() {
-        if (this.$store.state.band.isFailling)
-            this.bg = "bg-red-600"
-
-        else
-            this.bg = "bg-green-600"
-    },
-
-
     methods: {
         deleteBand() {
-            this.$store.commit('band/toggleBand')
-        } 
+            this.$store.commit('band/closeBand')
+        }
     }
 }
 </script>
