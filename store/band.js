2384bd2f3ca2b320a063cf44a5381482812f02d6
Rached <r.mejri74100@gmail.com>
2021-06-27 22:59:37 +0200

    fix: rewriting state routine in store

--- store/band.js
+++ store/band.js
@@ -1,13 +1,27 @@
 export const state = () => ({
     bandIsInvisible: true,
-    isFailling: true,
+    bgProp: "",
     text: ""
 })
   
 export const mutations = {
-    toggleBand (state, text, fail) {
-        state.bandIsInvisible = !state.bandIsInvisible
+    toggleBandAsFail (state, text) {
+        if (state.bandIsInvisible)
+            state.bandIsInvisible = false   
+
         state.text = text
-        state.isFailling = fail
+        state.bgProp = "bg-red-600"
     },
+
+    toggleBandAsSuccess(state, text) {
+        if (state.bandIsInvisible)
+            state.bandIsInvisible = false   
+
+        state.text = text
+        state.bgProp = "bg-green-600"
+    },
+
+    closeBand(state) {
+        state.bandIsInvisible = true   
+    }
 }
