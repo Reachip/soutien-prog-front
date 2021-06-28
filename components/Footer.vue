7d3e9a7494075ea14535878816aafc5067438f06
Rached <r.mejri74100@gmail.com>
2021-06-28 19:40:55 +0200

    fix: footer in the background when modal is open

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="footer bg-white relative pt-1">
+    <footer class="footer bg-white pt-1">
         <div class="container mx-auto px-6">
             <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
                 <div class="sm:w-2/3 text-center py-6">
