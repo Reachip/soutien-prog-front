59bfb43dcb77e6d327a09c112b69c41e2906e3e2
Rached <r.mejri74100@gmail.com>
2021-07-09 14:08:43 +0200

    fix: fixed footer

--- pages/index.vue
+++ pages/index.vue
@@ -5,67 +5,75 @@
     <ModalConnection />
     <ModalPurpose />
     <div class="bg-green-600 bg-yellow-600"></div>
-    <main>
-      <header class="bg-red-700 h-58 border">
-        <div>
-          <div
-            class="
-              flex flex-wrap
-              content-center
-              items-center
-              justify-center
-              my-6
-            "
-          >
-            <div class="mx-1">
-              <label class="font-bold text-xl text-white">Quand ?</label><br />
-              <input
-                type="date"
-                class="border rounded bg-gray-100 border-gray-200 p-2 my-2"
-                value="2020-03-22"
-                v-model="dateInput"
-                @change="filterCourse()"
-              />
-            </div>
+    <div class="flex flex-col h-screen justify-between">
+      <main>
+        <header class="bg-red-700 h-58 border">
+          <div>
+            <div
+              class="
+                flex flex-wrap
+                content-center
+                items-center
+                justify-center
+                my-6
+              "
+            >
+              <div class="mx-1">
+                <label class="font-bold text-xl text-white">Quand ?</label
+                ><br />
+                <input
+                  type="date"
+                  class="border rounded bg-gray-100 border-gray-200 p-2 my-2"
+                  value="2020-03-22"
+                  v-model="dateInput"
+                  @change="filterCourse()"
+                />
+              </div>
 
-            <div class="mx-1">
-              <label class="font-bold text-xl text-white"
-                >Pour quel module ?</label
-              ><br />
-              <select
-                class="
-                  min-w-full
-                  border
-                  rounded
-                  bg-gray-100
-                  border-gray-200
-                  p-2
-                  my-2
-                "
-                v-model="moduleInput"
-                @change="filterCourse()"
-              >
-                <option v-for="module in modules">
-                  {{ module.module_name }}
-                </option>
-              </select>
+              <div class="mx-1">
+                <label class="font-bold text-xl text-white"
+                  >Pour quel module ?</label
+                ><br />
+                <select
+                  class="
+                    min-w-full
+                    border
+                    rounded
+                    bg-gray-100
+                    border-gray-200
+                    p-2
+                    my-2
+                  "
+                  v-model="moduleInput"
+                  @change="filterCourse()"
+                >
+                  <option v-for="module in modules">
+                    {{ module.module_name }}
+                  </option>
+                </select>
+              </div>
             </div>
           </div>
+        </header>
+        <div v-for="course in filteredCourses">
+          <PropositionCard
+            :id="course.id"
+            :title="course.course_name"
+            :author="course.teacher"
+            :startAt="course.starting_at"
+            :endAt="course.ending_at"
+            :subject="course.description"
+          />
         </div>
-      </header>
-      <div v-for="course in filteredCourses">
-        <PropositionCard
-          :id="course.id"
-          :title="course.course_name"
-          :author="course.teacher"
-          :startAt="course.starting_at"
-          :endAt="course.ending_at"
-          :subject="course.description"
-        />
-      </div>
-      <p v-if="!filteredCourses || filteredCourses.length === 0" class="text-center text-xl text-red-700 mt-12">Aucun soutien n'a été trouvé pour aujourd'hui ...</p>
-    </main>
-    <Footer />
+        <p
+          v-if="!filteredCourses || filteredCourses.length === 0"
+          class="text-center text-xl text-red-700 mt-12"
+        >
+          Aucun soutien n'a été trouvé pour aujourd'hui ...
+        </p>
+      </main>
+      <Footer />
+    </div>
   </div>
 </template>
 
@@ -85,7 +93,7 @@ export default {
 
   async beforeMount() {
     this.courses = (await this.$axios.get("/course/")).data;
-    
+
     for (let index = 0; index < this.courses.length; index++) {
       try {
         const course = this.courses[index];
@@ -101,7 +109,7 @@ export default {
         ).format("DD-MM-YYYY HH:mm");
       } catch (why) {}
     }
-      
+
     this.filteredCourses = this.courses;
     this.modules = (await this.$axios.get("/module/")).data;
     this.$store.commit("courseData/setModules", this.modules);
